38a9f1f417ad8757f2b205bf300373295f1c6b39
Becheru888 <becheru93@gmail.com>
2019-05-21 15:01:12 +0100

    Flip card main section style added

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,23 @@
 const toggleMenu = () => {
-    menu.classList.toggle('menu--open')
-    // Toggle the "menu--open" class on your menu refence. 
-  };
-  
-  // Start Here: Create a reference to the ".menu" class
-  const menu = document.querySelector('.menu');
-  
-  // create a reference to the ".menu-button" class
-  const menuButton = document.querySelector('.menu-button');
-  // Using your menuButton reference, add a click handler that calls toggleMenu
-  menuButton.addEventListener('click', toggleMenu);
+  menu.classList.toggle('menu--open')
+
+};
+
+const menu = document.querySelector('.menu');
+
+const menuButton = document.querySelector('.menu-button');
+
+menuButton.addEventListener('click', toggleMenu);
+
+
+
+var card = document.querySelectorAll('.card');
+
+card.forEach(card => {
+  card.addEventListener( 'click', function() {
+    card.classList.toggle('is-flipped');
+  });
+})
+
+
+
