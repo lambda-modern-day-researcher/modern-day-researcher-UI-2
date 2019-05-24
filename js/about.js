561d8b2e07d9acf62de7468028da1a03e2179734
Becheru888 <becheru93@gmail.com>
2019-05-24 14:24:53 +0100

    turn button remove from the mobile view

--- js/about.js
+++ js/about.js
@@ -1,12 +1,13 @@
-class Obsj{
-    constructor(element){
-    this.element = element;
-    this.div = document.querySelectorAll('.more-details');
-    this.element.addEventListener('click', this.select.bind(this));
+class Obsj {
+    constructor(member) {
+        this.member = member;
+        this.div = document.querySelectorAll('.more-details');
+        this.member.addEventListener('click', this.select.bind(this));
+        this.personData = document.querySelectorAll('data-member')
     }
-    
-    select(){
-      
+
+    select() {
+
     }
 
 }
@@ -17,8 +18,8 @@ class Obsj{
 
 
 let details = document.querySelectorAll('.member');
-details.forEach(element => {
-    return new Obsj(element)
+details.forEach(member => {
+    return new Obsj(member)
 })
 
 
